bb0d654fe0151eaaac36c4d1ab6662932f18c23d
TehoraDutenu <meenahbass@hotmail.com>
2023-09-05 15:57:35 +0200

    end

---
+++ Mon TP/Mon TP/wp-content/uploads/aios/firewall-rules/settings.php
@@ -0,0 +1,6 @@
+<?php __halt_compiler();
+/**
+ * This file was created by All In One Security (AIOS) plugin.
+ * The file is required for storing and retrieving your firewall's settings.
+ */
+{"aios_enable_rename_login_page":"","aios_login_page_slug":"","aios_enable_brute_force_attack_prevention":"","aios_brute_force_secret_word":"","aios_cookie_based_brute_force_redirect_url":"http:\/\/127.0.0.1","aios_brute_force_attack_prevention_pw_protected_exception":"","aios_brute_force_attack_prevention_ajax_exception":"","aios_brute_force_secret_cookie_name":"aios_brute_force_secret_bf499e885499137cd97cc4cc3b3f8235","aios_brute_force_cookie_salt":"BI2O14\/5EO1-q@hp_eV:~tSwY0:)&B3mmbnw`;WmJ[t>sA;(VcrIfDgm+d?Rk_tjt]6DD[3wFL-62;vJFOD$kr}l<OhX*]67 o7 :zH*@sTNfE_.aRt`{ok7h;Y5b*Z{","aiowps_blacklist_user_agents":[],"aios_ip_retrieve_method":"","aiowps_ip_retrieve_method":"","aiowps_blacklist_ips":[]}
